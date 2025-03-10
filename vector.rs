10f7f1a10e723b3b9be799e537c9333b41e7bf98
ROHIT CHAUHAN <rohitchauhan404@outlook.com>
2025-03-10 19:33:06 +0530

    Update and rename decorator.py to vector.rs

---
+++ vector.rs
@@ -0,0 +1,21 @@
+fn main() {
+ 
+ let mut vec: Vec<i32> = vec![1, 2, 3, 4];
+ 
+ for i in &vec {
+     println!("{}", i);
+ }
+ 
+ vec.push(5);
+ println!("{:?}", vec);
+ 
+ let popped: Option<i32> = vec.pop();
+ 
+ match popped {
+     Some(value) =>  println!("Popped: {value}"),
+     None => println!("Index out of bounds"),
+ }
+ 
+ println!("{:?}", vec);
+ 
+}
