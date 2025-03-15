70f72cc07537ecc3c622b975a6eb253f226932d3
ROHIT CHAUHAN <rohitchauhan404@outlook.com>
2025-03-16 00:23:59 +0530

    multi_type vector with enum

--- vector.rs
+++ vector.rs
@@ -2,45 +2,44 @@
 // Rohit: Illustrating Vectors in Rust
 
 fn main() {
- 
- let mut vec: Vec<i32> = vec![1, 2, 3, 4];
- 
- for i in &vec {
-     println!("{i}");
- }
- 
- vec.push(5);
- println!("{vec:?}");
- 
- let popped: Option<i32> = vec.pop();
- 
- match popped {
-     Some(value) =>  println!("Popped: {value}"),
-     None => println!("Index out of bounds"),
- }
- 
- println!("{vec:?}");
+    let mut vec: Vec<i32> = vec![1, 2, 3, 4];
 
-// Creating a Vector with different types using Enum
-    enum Things {
+    for i in &vec {
+        println!("{i}");
+    }
+
+    vec.push(5);
+    println!("{vec:?}");
+
+    let popped: Option<i32> = vec.pop();
+
+    match popped {
+        Some(value) => println!("Popped: {value}"),
+        None => println!("Index out of bounds"),
+    }
+
+    println!("{vec:?}");
+
+    // Creating a Vector with different types using Enum
+    enum Thing {
         Apple(i32),
         Person(String),
         Exists(bool),
     }
 
     let mut things = vec![
-        Things::Apple(42),
-        Things::Person("Rohit".to_string()),
-        Things::Exists(true),
+        Thing::Apple(42),
+        Thing::Person("Rohit".to_string()),
+        Thing::Exists(true),
     ];
 
-    things.push(Things::Apple(56));
+    things.push(Thing::Apple(56));
 
     for thing in &things {
         match thing {
-            Things::Apple(count) => println!("Apple count: {}", count),
-            Things::Person(name) => println!("Person: {}", name),
-            Things::Exists(status) => println!("Exists: {}", status),
+            Thing::Apple(count) => println!("Apple count: {}", count),
+            Thing::Person(name) => println!("Person: {}", name),
+            Thing::Exists(status) => println!("Exists: {}", status),
         }
     }
 }
