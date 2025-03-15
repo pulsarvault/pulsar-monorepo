3c596fe11ff192f9b5ab8abf8d3aee07094746a6
ROHIT CHAUHAN <rohitchauhan404@outlook.com>
2025-03-16 00:19:47 +0530

    multi-type vectors with enums

--- vector.rs
+++ vector.rs
@@ -1,3 +1,6 @@
+#![warn(clippy::all)]
+// Rohit: Illustrating Vectors in Rust
+
 fn main() {
  
  let mut vec: Vec<i32> = vec![1, 2, 3, 4];
@@ -17,5 +20,27 @@ fn main() {
  }
  
  println!("{vec:?}");
- 
+
+// Creating a Vector with different types using Enum
+    enum Things {
+        Apple(i32),
+        Person(String),
+        Exists(bool),
+    }
+
+    let mut things = vec![
+        Things::Apple(42),
+        Things::Person("Rohit".to_string()),
+        Things::Exists(true),
+    ];
+
+    things.push(Things::Apple(56));
+
+    for thing in &things {
+        match thing {
+            Things::Apple(count) => println!("Apple count: {}", count),
+            Things::Person(name) => println!("Person: {}", name),
+            Things::Exists(status) => println!("Exists: {}", status),
+        }
+    }
 }
